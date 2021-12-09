a3ce4e35a7a651a25149cae198ec75c31213c36b
Juan Pablo R <dupjprgmail.com>
2021-12-09 15:48:51 -0500

    In this commit i have added a basic exercice with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  interface User {
+    name: string;
+  }
+
+  const [userName, setName] = useState<string>('')
+  const [lista, setLista] = useState<User[]>([])
+
+  const handleChange = (e: any) => {
+    const target = e.target;
+    setName(target.value);
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    setLista([...lista, { name: userName }]);
+    setName('');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {lista.map((item) => (
+        <div key={item.name}>{item.name}</div>
+      ))}
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input type="text" value={userName} onChange={(e) => handleChange(e)} />
+        <input type="submit" value='send' />
+      </form>
     </div>
   );
 }
